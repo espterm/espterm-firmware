b57e0ee75b55b33aa8c2b58a4238784086ff06d1
Ondřej Hruška <ondra@ondrovo.com>
2017-09-03 22:02:02 +0200

    change GitHub URL

--- user/version.h
+++ user/version.h
@@ -11,6 +11,6 @@
 
 #define FIRMWARE_VERSION STR(FW_V_MAJOR) "." STR(FW_V_MINOR) "." STR(FW_V_PATCH) "+" GIT_HASH
 #define FIRMWARE_VERSION_NUM (FW_V_MAJOR*10000 + FW_V_MINOR*100 + FW_V_PATCH) // this is used in ID queries
-#define TERMINAL_GITHUB_REPO "https://github.com/MightyPork/esp-vt100-firmware"
+#define TERMINAL_GITHUB_REPO "https://github.com/MightyPork/ESPTerm"
 
 #endif //ESP_VT100_FIRMWARE_VERSION_H
