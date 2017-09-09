6b603a1a8c338aa0e220acaeb63d433db8ba5f3e
Ondřej Hruška <ondra@ondrovo.com>
2017-09-09 19:42:29 +0200

    Delete old html files & moved front-end to a submodule

--- build_web.sh
+++ build_web.sh
@@ -7,17 +7,15 @@ mkdir -p html/img
 mkdir -p html/js
 mkdir -p html/css
 
-cd html_orig
-sh ./packjs.sh
-sh ./packcss.sh
-php ./build_html.php
+cd front-end
+sh ./build.sh
 cd ..
 
-cp html_orig/js/app.js html/js/
-cp html_orig/css/app.css html/css/
+cp front-end/js/app.js   html/js/
+cp front-end/css/app.css html/css/
 
-cp html_orig/img/* html/img/
-cp html_orig/favicon.ico html/favicon.ico
+cp front-end/img/*       html/img/
+cp front-end/favicon.ico html/favicon.ico
 
 # cleanup
 find html/ -name "*.orig" -delete
--- html_orig/packcss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Building css..."
-
-npm run sass -- --output-style compressed sass/app.scss > css/app.css
